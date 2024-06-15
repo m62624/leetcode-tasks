d4f51232d0a2daffec115632781dedee2d9dfd7b
Mansur <52788547+m62624@users.noreply.github.com>
2024-06-15 13:57:05 +0000

    [LeetCode Sync] Runtime - 31 ms (57.50%), Memory - 5.7 MB (12.50%)

--- hard/0502-ipo/solution.rs
+++ hard/0502-ipo/solution.rs
@@ -1,26 +1,26 @@
 use std::collections::BinaryHeap;
-use std::cmp::Reverse;
+
 
 
 impl Solution {
-   pub fn find_maximized_capital(k: i32, w: i32, profits: Vec<i32>, capital: Vec<i32>) -> i32 {
-        let mut projects: Vec<(i32, i32)> = capital.into_iter().zip(profits.into_iter()).collect();
-        projects.sort_by_key(|&(c, _)| c);
+ pub fn find_maximized_capital(k: i32, w: i32, profits: Vec<i32>, capital: Vec<i32>) -> i32 {
+        let mut projects: Vec<(&i32, &i32)> = capital.iter().zip(profits.iter()).collect();
+
+        projects.sort_unstable_by_key(|&(c, _)| c);
 
         let mut available_projects = BinaryHeap::new();
         let mut current_capital = w;
         let mut i = 0;
 
-        for _ in 0..k {
-            while i < projects.len() && projects[i].0 <= current_capital {
+        for _ in 1..=k {
+            while i < projects.len() && projects[i].0 <= &current_capital {
                 available_projects.push(projects[i].1);
                 i += 1;
             }
 
-            if let Some(max_profit) = available_projects.pop() {
-                current_capital += max_profit;
-            } else {
-                break;
+            match available_projects.pop() {
+                Some(p) => current_capital += p,
+                None => break,
             }
         }
 
